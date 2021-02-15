23e1f20c81b5732158f0b1e92bd6105a3db2c303
RyanFrazier <rfrazier716@gmail.com>
2021-02-15 16:59:35 -0500

    AnalyticRender WIP

--- adpd/packaging/renderers.py
+++ adpd/packaging/renderers.py
@@ -1,10 +1,11 @@
 from enum import Enum
 import adpd.packaging.simple_cg as cg
+import numpy as np
+
 
 def analytic_render(system):
+    pass
 
-
-# randers a system of surfaces and writes results to an SQLITE file
 
 class AnalyticRenderer(object):
     class States(Enum):
@@ -15,25 +16,25 @@ class AnalyticRenderer(object):
         IDLE = 4
         INITIALIZE = 5
 
-
-    def __init__(self, system=[], rays_per_source = 10, generation_limit = 10):
-        self._state = AnalyticRenderer.States.IDLE # by default the renderer is idling
+    def __init__(self, sources=[], system=[], rays_per_source=10, generation_limit=10):
+        self._state = AnalyticRenderer.States.IDLE  # by default the renderer is idling
         self._generation_number = 0
 
-        self._system = system # list of surfaces in the optical system
+        self._sources = sources
+        self._system = system  # list of surfaces in the optical system
         self._rays_per_source = rays_per_source
-        self._generation_limit = generation_limit # how many reflections/refractions a ray can encounter
+        self._generation_limit = generation_limit  # how many reflections/refractions a ray can encounter
 
-        self._state_functions = {
-            AnalyticRenderer.States.GENERATE : self._st_generate,
-            AnalyticRenderer.States.INITIALIZE : self._st_initialize,
-            AnalyticRenderer.States.PROPOGATE : self._st_propogate,
-            AnalyticRenderer.States.RECORD : self._st_record,
-            AnalyticRenderer.States.FINISH : self._st_finish,
+        self._state_machine = {
+            AnalyticRenderer.States.GENERATE: self._st_generate,
+            AnalyticRenderer.States.INITIALIZE: self._st_initialize,
+            AnalyticRenderer.States.PROPAGATE: self._st_propagate,
+            AnalyticRenderer.States.RECORD: self._st_record,
+            AnalyticRenderer.States.FINISH: self._st_finish,
         }
 
     def reset(self):
-        self._state = AnalyticRenderer.States.IDLE # by default the renderer is idling
+        self._state = AnalyticRenderer.States.IDLE  # by default the renderer is idling
         self._generation_number = 0
 
     def set_generation_limit(self, limit):
@@ -46,25 +47,24 @@ class AnalyticRenderer(object):
         self._system = system
 
     def render(self):
-        self._state = AnalyticRenderer.States.INITIALIZE # kick off the state machine
+        self._state = AnalyticRenderer.States.INITIALIZE  # kick off the state machine
 
         # run the state machine to completion
-        while self._state!= AnalyticRenderer.States.IDLE:
-            self._state_machine[self._state]() # execute the state function for that state
+        while self._state != AnalyticRenderer.States.IDLE:
+            self._state_machine[self._state]()  # execute the state function for that state
 
     def _st_initialize(self):
-        self.reset() # reset the renderer states/generation number
+        self.reset()  # reset the renderer states/generation number
 
         # iterate over components in system, if they have a function to generate rays, call it
         attr_string = 'generate_rays'
 
-        generate_fns = [getattr(item, attr_string) for item in self._system if hasattr(item, attr_string)]
-        n_rays = self._rays_per_source*len(generate_fns) # how many rays to create for the system
-        self._rays = cg.bundle_of_rays(n_rays) # allocate the ray matrix
+        n_rays = self._rays_per_source * len(self._sources)  # how many rays to create for the system
+        self._rays = cg.bundle_of_rays(n_rays)  # allocate the ray matrix
 
         # fill the matrix with rays
-        for n,fn in enumerate(generate_fns):
-            self._rays[:(n+1)*n_rays] = fn(self._rays_per_source)
+        for n, source in enumerate(self._sources):
+            self._rays[:,:,n*self._rays_per_source:(n + 1) * self._rays_per_source] = source.generate_rays(self._rays_per_source)
 
         self._state = self.States.PROPAGATE  # update the state machine to propagate through the system
 
@@ -72,13 +72,26 @@ class AnalyticRenderer(object):
         pass
 
     def _st_propagate(self):
-        pass
+        hits_matrix = np.zeros((len(self._system), self._rays.shape[-1]))
+        for n, surface in enumerate(self._system):
+            hits_matrix[n] = surface.intersect(self._rays)
+
+        hit_distances = np.min(hits_matrix, axis=0)
+        hit_surfaces = np.where(np.isfinite(hit_distances), np.argmin(hits_matrix, axis=0), -1)
+        # next need to call the shader function for each surface and trim the dead rays
+        intersection_points = self._rays[0] + hit_distances*self._rays[1]
+        intersection_rays = np.array((intersection_points, self._rays[1])) # make a list of rays where they intersect
+
+        for n, surface in enumerate(self._system):
+            new_rays, new_indices = surface.shade(intersection_rays[:,:,hit_surfaces == n], 1, 0)
+            intersection_rays[:, :, hit_surfaces == n] = new_rays
+
+        self._generation_number+=1
+        self._state = self.States.PROPAGATE if self._generation_number < self._generation_limit else self.States.IDLE
+
 
     def _st_record(self):
         pass
 
     def _st_finish(self):
         pass
-
-
-
--- test/test_renderers.py
+++ test/test_renderers.py
@@ -1,9 +1,24 @@
 import unittest
+import adpd.packaging.renderers as render
+from adpd.packaging.components.sources import LineOfRays
+from adpd.packaging.analytic_surface import YZPlane
+import adpd.packaging.shaders.analytic as mat
 
 
 class TestAnalyticRenderer(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self) -> None:
+        self.sources = (LineOfRays().rotate_y(-45), LineOfRays().rotate_y(-135))
+        self.system = (YZPlane(material=mat.mirror).move_x(3), YZPlane(material = mat.mirror).move_x(-3))
+
+    def test_creation(self):
+        # make a basic system  with a single generation limit
+        this_renderer = render.AnalyticRenderer(self.sources, self.system, 1, generation_limit=1)
+        this_renderer.render()
+
+    def test_full_render(self):
+        # make a basic system  with a single generation limit
+        this_renderer = render.AnalyticRenderer(self.sources, self.system, 1000, generation_limit=10)
+        this_renderer.render()
 
 
 if __name__ == '__main__':
