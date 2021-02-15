a9c5207ab9b79ec377ca04bc988a70fbee73a606
RyanFrazier <rfrazier716@gmail.com>
2021-02-15 16:17:33 -0500

    added component package has sources module for ray sources wrote tests for line of rays

---
+++ adpd/packaging/components/sources.py
@@ -0,0 +1,40 @@
+import adpd.packaging.simple_cg as cg
+import numpy as np
+import abc
+
+
+class Source(cg.WorldObject, abc.ABC):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def generate_rays(self, n_rays):
+        local_rays = self._local_ray_generation(n_rays)
+        world_rays = np.matmul(self._world_coordinate_transform, local_rays) # transform rays to world space
+        world_rays[1] /= np.linalg.norm(world_rays[1], axis=0)  # normalize the direction vector
+        return world_rays
+
+    @abc.abstractmethod
+    def _local_ray_generation(self, n_rays):
+        pass
+
+
+class LineOfRays(Source):
+
+    def __init__(self, spacing=1, *args, **kwargs):
+        self._spacing = spacing
+        super().__init__(*args, **kwargs)
+
+    def _local_ray_generation(self, n_rays):
+        """
+        creates a line of rays directed towards the positive x-axis along the y-axis
+        :param n_rays:
+        :return:
+        """
+        rays = cg.bundle_of_rays(n_rays)
+        # if we want more than one ray, linearly space them, otherwise default position is fine
+        if n_rays>1:
+            ray_position = np.linspace(-self._spacing / 2, self._spacing / 2, n_rays)
+            rays[0, 1] = ray_position  # space rays along the y-axis
+        rays[1, 0] = 1  # direct rays along positive x
+        return rays
---
+++ test/test_components/test_sources.py
@@ -0,0 +1,54 @@
+import unittest
+import numpy as np
+import adpd.packaging.components.sources as sources
+import adpd.packaging.simple_cg as cg
+
+
+class TestLineOfRays(unittest.TestCase):
+    def setUp(self) -> None:
+        self.source = sources.LineOfRays(1)
+
+    def test_single_ray_generation(self):
+        rays = self.source.generate_rays(1)
+        self.assertEqual(rays.shape, (2, 4, 1))
+
+        expected_pos = np.atleast_2d(cg.Point())
+        self.assertTrue(np.allclose(rays[0], expected_pos.T))
+
+        expected_dir = np.atleast_2d(cg.Vector(1, 0, 0))
+        self.assertTrue(np.allclose(rays[1], expected_dir.T))
+
+    def test_multi_ray_generation(self):
+        n_rays = 3
+        rays = self.source.generate_rays(n_rays)
+        self.assertEqual(rays.shape, (2, 4, n_rays))
+
+        expected_pos = np.array((-0.5, 0, 0.5))
+        self.assertTrue(np.allclose(rays[0, 1], expected_pos))
+
+        self.assertTrue(np.allclose(rays[1, 0], 1))
+        self.assertTrue(np.allclose(rays[1, 1], 0))
+        self.assertTrue(np.allclose(rays[1, 2], 0))
+
+    def test_transformed_ray_generation(self):
+        # rotate and generate a single ray
+        self.source.rotate_y(-90)
+        rays = self.source.generate_rays(1)
+
+        expected_dir = np.atleast_2d(cg.Vector(0, 0, 1))
+        self.assertTrue(np.allclose(rays[1], expected_dir.T))
+
+    def test_translated_ray_generation(self):
+        # rotate and generate a single ray
+        self.source.move_z(5)
+        rays = self.source.generate_rays(1)
+
+        expected_pos = np.atleast_2d(cg.Point(0, 0, 5))
+        self.assertTrue(np.allclose(rays[0], expected_pos.T))
+
+        expected_dir = np.atleast_2d(cg.Vector(1, 0, 0))
+        self.assertTrue(np.allclose(rays[1], expected_dir.T))
+
+
+if __name__ == '__main__':
+    unittest.main()
