ce2e9e9c0925e10d44dacf6495bca6898170199e
RyanFrazier <rfrazier716@gmail.com>
2021-02-19 17:29:49 -0500

    changing AnalyticSystem to a dataclass object need to change tests

--- pyrayt/designer.py
+++ pyrayt/designer.py
@@ -1,37 +1,10 @@
 import pyrayt.simple_cg as cg
 import collections
+from dataclasses import dataclass
 import re
 
 
-def _multi_string_replace(string, replacements: dict):
-    rep_sorted = sorted(replacements, key=len, reverse=True)
-    rep_escaped = map(re.escape, rep_sorted)
-
-    # Create a big OR regex that matches any of the substrings to replace
-    pattern = re.compile("|".join(rep_escaped))
-
-    # For each match, look up the new string in the replacements, being the key the normalized old string
-    return pattern.sub(lambda match: replacements[match.group(0)], string)
-
-
-class AnalyticSystem(collections.UserDict):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)  # call the next constructor in the MRO
-
-        # create default object groups that can be written to
-        self.data["sources"] = cg.ObjectGroup()
-        self.data["components"] = cg.ObjectGroup()
-        self.data["detectors"] = cg.ObjectGroup()
-
-        self._flattened_system = list() # a placeholder for the flattened optical system
-
-    def flatten(self):
-        # reset the flattened_system
-        self._flattened_system = []
-        self._flatten_helper(self.data.values(), self._flattened_system)
-        return self._flattened_system
-
-    @staticmethod
+def flatten(list_to_flatten):
     def _flatten_helper(iterable, flattened_list):
         """
         a helper method to walk through a nested list and return the flattened list in order
@@ -43,9 +16,34 @@ class AnalyticSystem(collections.UserDict):
         for item in iterable:
             # if the item is itself iterable, recursively enter the function
             if not isinstance(item, str) and hasattr(item, '__iter__'):
-                AnalyticSystem._flatten_helper(item, flattened_list)
+                _flatten_helper(item, flattened_list)
             else:
                 flattened_list.append(item)
 
+    # reset the flattened_system
+    flattened_list = []
+    _flatten_helper(list_to_flatten, flattened_list)
+    return flattened_list
 
 
+@dataclass(unsafe_hash=True)
+class AnalyticSystem(object):
+    sources: cg.ObjectGroup = cg.ObjectGroup()
+    components: cg.ObjectGroup = cg.ObjectGroup()
+    detectors: cg.ObjectGroup = cg.ObjectGroup()
+
+
+# class AnalyticSystem(collections.UserDict):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)  # call the next constructor in the MRO
+#
+#         # create default object groups that can be written to
+#         self.data["sources"] = cg.ObjectGroup()
+#         self.data["components"] = cg.ObjectGroup()
+#         self.data["detectors"] = cg.ObjectGroup()
+#
+#         self._flattened_system = list()  # a placeholder for the flattened optical system
+#
+#     @property
+#     def sources(self):
+#         return self.data["source"]
--- test/test_designer.py
+++ test/test_designer.py
@@ -2,9 +2,29 @@ import unittest
 import pyrayt.designer as designer
 import pyrayt.simple_cg as cg
 
+
+class TestFlattenFn(unittest.TestCase):
+    def test_flatten_system(self):
+        system = []
+        system.append(0)
+        system.append((1, (2, 3)))
+        system.append((4, 5, 6))
+        system.append(7)
+        flattened = designer.flatten(system)
+        for n,field in enumerate(flattened):
+            self.assertEqual(n,field)
+
 class TestAnalyticSystem(unittest.TestCase):
     def setUp(self):
         self.system = designer.AnalyticSystem()
+
+    def test_calling_fields(self):
+        fields = ("sources", "components", "detectors")
+        for field in fields:
+            attr = getattr(self.system, field)
+            self.assertTrue(isinstance(attr, cg.ObjectGroup))
+
+        print(self.system)
 
     def test_calling_dict_fields(self):
         # the three default fields should be created with the object
@@ -21,15 +41,6 @@ class TestAnalyticSystem(unittest.TestCase):
 
         self.system["components"].append(my_object)
         self.assertEqual(self.system["components"][0], my_object)
-
-    def test_flatten_system(self):
-        self.system["sources"].append(0)
-        self.system["sources"].append((1, (2, 3)))
-        self.system["components"].append((4, 5, 6))
-        self.system["detectors"].append(7)
-        flattened = self.system.flatten()
-        for n,field in enumerate(flattened):
-            self.assertEqual(n,field)
 
 
 if __name__ == '__main__':
