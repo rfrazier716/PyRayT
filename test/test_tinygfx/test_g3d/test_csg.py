bcd7d769d23145388f27bd89e42d96997cd164a4
RyanFrazier <rfrazier716@gmail.com>
2021-03-13 22:58:20 -0500

    CSG WIP

--- test/test_tinygfx/test_g3d/test_csg.py
+++ test/test_tinygfx/test_g3d/test_csg.py
@@ -9,7 +9,7 @@ class TestCSGAdd(unittest.TestCase):
     def setUp(self) -> None:
         self.l_shape = cg.Sphere(1)
         self.r_shape = cg.Sphere(1)
-        self.csg = cg.CSGSurface(self.l_shape, self.r_shape, cg.CSGSurface.operations.ADD)
+        self.csg = csg.CSGSurface(self.l_shape, self.r_shape, csg.Operation.UNION)
 
     def test_bounding_box_updating(self):
         spans = self.csg.bounding_box.axis_spans
@@ -17,17 +17,32 @@ class TestCSGAdd(unittest.TestCase):
         self.assertTrue(np.allclose(spans, expected_spans))
 
         # if we move a child surface the bounding box should update
-        self.r_shape.move_x(3)
+        self.r_shape.move_x(1)
         spans = self.csg.bounding_box.axis_spans
-        expected_spans = np.array(((-1, -1, -1), (4, 1, 1))).T
+        expected_spans = np.array(((-1, -1, -1), (2, 1, 1))).T
         self.assertTrue(np.allclose(spans, expected_spans))
+
+    def test_intersection(self):
+        n_rays = 10
+        self.r_shape.move_y(-1)
+        rays = cg.bundle_of_rays(n_rays)
+        rays[1, 0] = 1  # point towards the right axis
+        rays[0, 0] = -5
+        rays[0, 1] = np.linspace(-2, 2, n_rays)
+
+        hits = self.csg.intersect(rays)
+
+
+    def test_moving_to_nonintersection(self):
+        with self.assertRaises(ValueError):
+            self.r_shape.move(4)
 
 
 class TestCSGIntersect(unittest.TestCase):
     def setUp(self) -> None:
         self.l_shape = cg.Sphere(1)
         self.r_shape = cg.Sphere(1)
-        self.csg = cg.CSGSurface(self.l_shape, self.r_shape, cg.CSGSurface.operations.INT)
+        self.csg = csg.CSGSurface(self.l_shape, self.r_shape, csg.Operation.INTERSECT)
 
     def test_bounding_box_updating(self):
         spans = self.csg.bounding_box.axis_spans
@@ -39,6 +54,10 @@ class TestCSGIntersect(unittest.TestCase):
         spans = self.csg.bounding_box.axis_spans
         expected_spans = np.array(((-0, -1, -1), (1, 1, 1))).T
         self.assertTrue(np.allclose(spans, expected_spans), f"expected {expected_spans}, got {spans}")
+
+    def test_moving_to_nonintersection(self):
+        with self.assertRaises(ValueError):
+            self.r_shape.move(4)
 
 
 class TestArrayCSGOperation(unittest.TestCase):
--- tinygfx/g3d/csg.py
+++ tinygfx/g3d/csg.py
@@ -1,6 +1,8 @@
 from enum import Enum
 import numpy as np
 
+from tinygfx.g3d import Intersectable, primitives as primitives
+
 
 class Operation(Enum):
     UNION = 1
@@ -11,8 +13,8 @@ class Operation(Enum):
 def array_csg(array1: np.ndarray, array2: np.ndarray, operation: Operation):
     """
     Given two arrays and an operation, returns a new array which is the CSG operation acting on the array.
-    If the array is thought of as intersection points between a ray and a to objects being combined with a CSG
-    operation, the result is the valid hits for the resulting object. Function assumes both arrays are sorted and have
+    If the array is thought of as intersection points between a ray and a two objects being combined with a CSG
+    operation, the returned is the valid hits for the resulting object. Function assumes both arrays are sorted and have
     an even number of axis=0 elements
 
     :param array1:
@@ -38,13 +40,11 @@ def array_csg(array1: np.ndarray, array2: np.ndarray, operation: Operation):
         merged_array = np.sort(merged_array, axis=0)  # sort the merged array since we don't need it anymore
         merged_mask = merged_mask[merged_argsort, np.arange(merged_mask.shape[-1])]
         ray_hit_path = np.cumsum(merged_mask, axis=0)
-        print(ray_hit_path)
 
-        if ray_hit_path[-1] != 0:
+        if np.any(ray_hit_path[-1], 0):
             raise ValueError("Invalid Ray Traversal, Shape was never exited?")
         # for a union operation you want the longest spans from 1-0, do this by casting all numbers >1 to zero
         ray_hit_path = np.where(ray_hit_path > 1, 1, ray_hit_path)
-        print(ray_hit_path)
         ray_hit_path = np.diff(ray_hit_path, axis=0, prepend=0)
         return np.sort(np.where(ray_hit_path != 0, merged_array, np.inf), axis=0)
 
@@ -59,7 +59,7 @@ def array_csg(array1: np.ndarray, array2: np.ndarray, operation: Operation):
         merged_mask = merged_mask[merged_argsort, np.arange(merged_mask.shape[-1])]
         ray_hit_path = np.cumsum(merged_mask, axis=0)
 
-        if ray_hit_path[-1] != 0:
+        if np.any(ray_hit_path[-1]) != 0:
             raise ValueError("Invalid Ray Traversal, Shape was never exited?")
 
         # for an intersection operation we want to find where the ray hit path is 2 and take the next point
@@ -83,8 +83,72 @@ def array_csg(array1: np.ndarray, array2: np.ndarray, operation: Operation):
 
         # first, cast anything that isn't a 1 to a zero
         ray_hit_path = np.where(ray_hit_path != 1, 0, ray_hit_path)
-        print(ray_hit_path)
 
         # now add together the ray hit path with itself, rolled one down
         ray_hit_path += np.roll(ray_hit_path, 1, axis=0)
         return np.sort(np.where(ray_hit_path == 1, merged_array, np.inf), axis=0)
+
+
+class CSGSurface(Intersectable):
+
+    def __init__(self, l_child: Intersectable, r_child: Intersectable, operation: Operation, *args,
+                 **kwargs):
+        super().__init__(*args, **kwargs)  # call the next constructor in the MRO
+
+        # store the boolean type
+        self._operation = operation
+
+        # attach the child objects to the parent
+        # make it so if a child is modified the bounding boxes are recalculated
+        self._l_child = l_child
+        self._l_child.attach_to(self)
+        self._l_child.var_watchlist.append(self._update_bounding_box)
+
+        self._r_child = r_child
+        self._r_child.attach_to(self)
+        self._r_child.var_watchlist.append(self._update_bounding_box)
+
+        self._update_bounding_box()  # update the bounding box
+
+    def _update_bounding_box(self):
+        """
+        the bounding box is only updated for add and intersection operations
+        :return:
+        """
+        if self._operation != Operation.DIFFERENCE:
+            new_spans = array_csg(self._l_child.bounding_box.axis_spans.T,
+                                  self._r_child.bounding_box.axis_spans.T,
+                                  self._operation)
+
+            # check if the resulting spans are valid
+            if (self._operation == Operation.UNION and np.any(np.isfinite(new_spans[2:]))) or \
+                    (self._operation == Operation.INTERSECT and np.any(np.all(np.isinf(new_spans), axis=0))):
+                raise ValueError(f"CSG Child Surfaces {self._l_child} and {self._r_child} no longer intersect")
+
+            self._aobb = primitives.Cube(*new_spans[:2])
+
+        else:
+            # for a diff operation the bounding box must always be the bounding box of the left hand child
+            self._aobb = self._l_child.bounding_box
+
+    def intersect(self, rays):
+        # steps for intersection
+        # 1 check that the rays intersect the bounding box
+        # for the rays that do, call the l_child and r_child intersect functions
+        # depending on the CSG operation reject certain hits, and return
+
+        # get a boolean mask of rays that intersect the surface
+        rays = np.atleast_3d(rays)
+        bounding_box_intersections = np.any(np.isfinite(self._aobb.intersect(rays)), axis=0)
+
+        # calculate the csg hits for the array subset that intersects the object
+        intersecting_rays = rays[:,:,bounding_box_intersections]  # make a subset of rays that intersect
+        local_ray_set = np.matmul(self._get_object_transform(), intersecting_rays)
+        l_hits = self._l_child.intersect(local_ray_set)
+        r_hits = self._r_child.intersect(local_ray_set)
+        csg_hits = array_csg(l_hits, r_hits, self._operation)
+
+        # plug the csg_hits back into a main hit matrix
+        all_hits = np.full((csg_hits.shape[0], rays.shape[-1]), np.inf)
+        all_hits[:,bounding_box_intersections] = csg_hits  # plug the csg_hits into the hits matrix
+        return all_hits  # return the hits matrix
