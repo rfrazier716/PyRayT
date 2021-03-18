97c7ee6d9459da5afc07ccf39d479765e8aff764
RyanFrazier <rfrazier716@gmail.com>
2021-03-17 21:32:31 -0400

    minor changes to how array_csg works

--- tinygfx/g3d/csg.py
+++ tinygfx/g3d/csg.py
@@ -23,73 +23,34 @@ def array_csg(array1: np.ndarray, array2: np.ndarray, operation: Operation, sort
     :return:
     """
 
-    # we're going to stack both array into one that will be sorted
-
-    if array1.ndim == 1:  # if a 1d array was passed, reshape the joined array appropriately
-        merged_array = np.atleast_2d(np.hstack((array1, array2))).T
+    if array1.ndim == 1:
+        # if 1D arrays were passed, concatenate
+        merged_array = np.concatenate(array1, array2)
     else:
+        # otherwise stack them where each column represents a unique ray's hits
         merged_array = np.vstack((array1, array2))
 
-    if operation == Operation.UNION:
-        # make a mask that tracks when the ray enters and exits each surface
-        merged_mask = np.ones(merged_array.shape)
-        merged_mask[1::2] = -1
-
-        # take the argsort along axis0 to sort the hits, this will be used to reshape the mask
-        merged_argsort = np.argsort(merged_array, axis=0)
-        merged_array = np.sort(merged_array, axis=0)  # sort the merged array since we don't need it anymore
-        merged_mask = merged_mask[merged_argsort, np.arange(merged_mask.shape[-1])]
+    merged_argsort = np.argsort(merged_array, axis=0)
+    merged_array = merged_array[merged_argsort, np.arange(merged_array.shape[-1])]
+    if operation == Operation.UNION or operation == Operation.INTERSECT:
+        merged_mask = np.where(merged_argsort & 1, -1, 1)
         ray_hit_path = np.cumsum(merged_mask, axis=0)
 
-        if np.any(ray_hit_path[-1], 0):
-            raise ValueError("Invalid Ray Traversal, Shape was never exited?")
-        # for a union operation you want the longest spans from 1-0, do this by casting all numbers >1 to zero
+    elif operation == Operation.DIFFERENCE:
+        merged_mask = np.where(np.logical_xor(merged_argsort & 1, merged_argsort >= array1.shape[0]), -1, 1)
+        ray_hit_path = np.cumsum(merged_mask, axis=0) + 1
+    else:
+        raise ValueError(f"operation {operation} is invalid")
+
+    if operation == Operation.UNION:
         ray_hit_path = np.where(ray_hit_path > 1, 1, ray_hit_path)
         ray_hit_path = np.diff(ray_hit_path, axis=0, prepend=0)
         csg_hits = np.where(ray_hit_path != 0, merged_array, np.inf)
 
-    elif operation == Operation.INTERSECT:
-        # make a mask that tracks when the ray enters and exits each surface
-        merged_mask = np.ones(merged_array.shape)
-        merged_mask[1::2] = -1
-
-        # take the argsort along axis0 to sort the hits, this will be used to reshape the mask
-        merged_argsort = np.argsort(merged_array, axis=0)
-        merged_array = np.sort(merged_array, axis=0)  # sort the merged array since we don't need it anymore
-        merged_mask = merged_mask[merged_argsort, np.arange(merged_mask.shape[-1])]
-        ray_hit_path = np.cumsum(merged_mask, axis=0)
-
-        if np.any(ray_hit_path[-1]) != 0:
-            raise ValueError("Invalid Ray Traversal, Shape was never exited?")
-
-        # for an intersection operation we want to find where the ray hit path is 2 and take the next point
-
-        ray_hit_path = np.where(ray_hit_path == 2, 1, 0)
-        ray_hit_path += np.roll(ray_hit_path, 1, axis=0)
-        csg_hits = np.where(ray_hit_path == 1, merged_array, np.inf)
-
-    elif operation == Operation.DIFFERENCE:
-        # A difference operation is the same as the union operate of A with the inverse of B.
-
-        merged_mask = np.ones(merged_array.shape)
-        merged_mask[1:array1.shape[0]:2] = -1
-        merged_mask[array1.shape[0]::2] = -1
-
-        # take the argsort along axis0 to sort the hits, this will be used to reshape the mask
-        merged_argsort = np.argsort(merged_array, axis=0)
-        merged_array = np.sort(merged_array, axis=0)  # sort the merged array since we don't need it anymore
-        merged_mask = merged_mask[merged_argsort, np.arange(merged_mask.shape[-1])]
-        ray_hit_path = np.cumsum(merged_mask, axis=0)
-
-        # first, cast anything that isn't a 1 to a zero
-        ray_hit_path = np.where(ray_hit_path != 1, 0, ray_hit_path)
-
-        # now add together the ray hit path with itself, rolled one down
-        ray_hit_path += np.roll(ray_hit_path, 1, axis=0)
-        csg_hits = np.where(ray_hit_path == 1, merged_array, np.inf)
-
-    else:
-        raise ValueError(f"Invalid Operation provided {operation}")
+    elif operation == Operation.INTERSECT or operation == Operation.DIFFERENCE:
+        is_two = (ray_hit_path == 2)
+        mask = np.logical_or(is_two, np.roll(is_two, 1, axis=0))
+        csg_hits = np.where(mask, merged_array, np.inf)
 
     return np.sort(csg_hits, axis=0) if sort_output else csg_hits
 
@@ -103,7 +64,6 @@ class CSGSurface(Intersectable):
         # store the boolean type
         self._operation = operation
         self.var_watchlist.append(self._update_bounding_box)
-
 
         # attach the child objects to the parent
         # make it so if a child is modified the bounding boxes are recalculated
