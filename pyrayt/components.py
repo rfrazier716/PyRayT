fixed formula for calculating thick lens width

pyrayt/components.py
@@ -132,11 +132,11 @@ def _lens_full_thickness(r1, r2, thickness, aperture) -> Tuple[float, float]:
     # The left thickness is based on the radius of curvature, positive is concave, negative is convex
     left_thickness = thickness/2
     if np.isfinite(r1) and r1 < 0:
-        left_thickness += -(r1 + np.sqrt(r1**2 - (max_height)**2))
+        left_thickness += np.abs(r1)-np.sqrt(np.abs(r1)**2 - max_height**2)
     
     right_thickness = thickness / 2
     if np.isfinite(r2) and r2 > 0:
-        right_thickness += r2 - np.sqrt(r2**2 - (max_height)**2)
+        right_thickness += np.abs(r2)-np.sqrt(np.abs(r2)**2 - max_height**2)
 
     center_shift = right_thickness - left_thickness
     total_thickness = right_thickness + left_thickness

test/test_pyrayt/test_components/test_components.py
@@ -16,24 +16,24 @@ class TestThickLensAperture(unittest.TestCase):
         self.assertEqual(total_thickness, thickness)
         self.assertAlmostEqual(offset, 0.0)
     
-    def test_balanced_convex_aperture(self):
+    def test_balanced_concave_aperture(self):
         r1 = -100
         r2 = 100
         thickness = .1
         aperture = 1
-        expected_thickness = thickness + 2*(aperture/2)**2/(8*r2)
+        expected_thickness = thickness + (aperture/2)**2/(r2)
         total_thickness, offset = pyrayt.components._lens_full_thickness(r1, r2, thickness, aperture)
         self.assertAlmostEqual(total_thickness, expected_thickness)
         self.assertAlmostEqual(offset, 0.0)
     
-    def test_imbalanced_convex_aperture(self):
+    def test_imbalanced_concave_aperture(self):
         r1 = -100
         r2 = 50
         thickness = .1
         aperture = 1
-        expected_thickness = thickness + (aperture/2)**2/(8*np.abs(r1)) + (aperture/2)**2/(8*np.abs(r2))
-        expected_offset = (aperture/2)**2/(8*np.abs(r2)) - (aperture/2)**2/(8*np.abs(r1))
+        expected_thickness = thickness + 0.5*(aperture/2)**2/(np.abs(r1)) + 0.5*(aperture/2)**2/(np.abs(r2))
+        expected_offset = 0.5*(aperture/2)**2/(np.abs(r2)) - 0.5*(aperture/2)**2/(np.abs(r1))
         total_thickness, offset = pyrayt.components._lens_full_thickness(r1, r2, thickness, aperture)
-        self.assertAlmostEqual(total_thickness, expected_thickness)
-        self.assertAlmostEqual(offset, expected_offset)
+        self.assertTrue(np.isclose(total_thickness, expected_thickness, rtol=1E-3))
+        self.assertTrue(np.isclose(offset, expected_offset, rtol=1E-3))
 